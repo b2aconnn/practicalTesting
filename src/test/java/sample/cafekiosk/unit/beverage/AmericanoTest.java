c4577bb42de9d640346717ff76ee350f1535da6f
b2aconnn <b2aconnn@gmail.com>
2024-10-19 13:50:55 +0900

    feat(section-3): JUnit5로 테스트하기

---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,24 @@
+package sample.cafekiosk.unit.beverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmericanoTest {
+    Americano americano = new Americano();
+
+    @Test
+    void getName() {
+//        assertEquals(americano.getName(), "아메리카노");
+
+        // 좀 더 명시적인 문법임
+        assertThat(americano.getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void getPrice() {
+        Americano americano = new Americano();
+        assertThat(americano.getPrice()).isEqualTo(4000);
+    }
+}
